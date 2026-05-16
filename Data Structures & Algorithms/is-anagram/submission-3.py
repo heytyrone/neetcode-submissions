cc7b07722be7c566dfbda3d981ba61e1fd6f37af
Tyrone Li <heytyrone@gmail.com>
2026-05-16 13:53:42 -0400

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        s_dict = self.freq_dict(s)
+        t_dict = self.freq_dict(t)
+        return s_dict == t_dict
+
+    def freq_dict(self, s: str) -> dict:
+        s_dict = {}
+        for letter in s:
+            if s_dict.get(letter) == None:
+                s_dict[letter] = 1
+            else:
+                s_dict[letter] +=1
+        return s_dict
+            
